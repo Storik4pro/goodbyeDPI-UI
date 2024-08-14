85c9c30714375a133dbf7dbc511cd2ad6975a60f
Storik4 <serstor15@gmail.com>
2024-08-14 19:35:49 +0300

    adding toasted support

--- src/main.py
+++ src/main.py
@@ -1,9 +1,13 @@
+import asyncio
 from tkinter import messagebox
 import sys
 import subprocess
 import os
 import ctypes
 import psutil
+import requests
+from toasted import ToastDismissReason
+from win11toast import notify, update_progress, toast, clear_toast, toast_async
 
 def run_as_admin(cmd, cwd):
     try:
@@ -34,8 +38,8 @@ else:
     import ctypes.wintypes
     import darkdetect
     from customtkinter import *
-    from _data import settings, SETTINGS_FILE_PATH, GOODBYE_DPI_PATH, FONT, DEBUG, DIRECTORY, text
-    from utils import install_font, start_process, download_blacklist
+    from _data import settings, SETTINGS_FILE_PATH, GOODBYE_DPI_PATH, FONT, DEBUG, DIRECTORY, REPO_NAME, REPO_OWNER, BACKUP_SETTINGS_FILE_PATH, text
+    from utils import install_font, start_process, download_blacklist, move_settings_file, ProgressToast, register_app, show_message, show_error
     import pywintypes
     import configparser
     from win10toast_click import ToastNotifier
@@ -45,8 +49,10 @@ else:
     from PIL import Image, ImageTk
     import threading
 
+    
+
     first_run = settings.settings['GLOBAL']['is_first_run']
-    install_font_result = True
+    install_font_result = False
     if not DEBUG:
         existing_app = is_process_running('goodbyeDPI.exe')
         if existing_app:
@@ -57,17 +63,44 @@ else:
             else:
                 sys.exit(0)
         if first_run == 'True':
+            register_app()
+            try:
+                config_old = configparser.ConfigParser()
+                config_old.read(BACKUP_SETTINGS_FILE_PATH)
+
+                config = configparser.ConfigParser()
+                config.read(SETTINGS_FILE_PATH)
+
+                for section in config.sections():
+                    if not config_old.has_section(section):
+                        config_old.add_section(section)
+                    for key, value in config.items(section):
+                        if not config_old.has_option(section, key):
+                            config_old.set(section, key, value)
+                with open(SETTINGS_FILE_PATH, 'w') as configfile:
+                    config_old.write(configfile)
+                settings.reload_settings()
+                config = config_old
+            except:
+                config = configparser.ConfigParser()
+                config.read(SETTINGS_FILE_PATH)
+
             install_font_result = install_font('data/font/Nunito-SemiBold.ttf')
-            config = configparser.ConfigParser()
-            config.read(SETTINGS_FILE_PATH)
+            
             config['GLOBAL']['is_first_run'] = 'False'
             with open(SETTINGS_FILE_PATH, 'w') as configfile:
                 config.write(configfile)
             settings.reload_settings()
 
-    version = "1.0.3"
+    version = "1.0.4"
     
+    def get_latest_release():
+        url = f"https://api.github.com/repos/{REPO_OWNER}/{REPO_NAME}/releases/latest"
+        response = requests.get(url)
+        data = response.json()
+        latest_version = data["tag_name"]
 
+        return latest_version
 
     regions = {
         text.inAppText['ru']:'RU',
@@ -81,6 +114,7 @@ else:
             self.title(f'goodbyeDPI UI - v {version}')
             self.resizable(False, False)
 
+            self.notification_thread = None
             self.is_update = False
 
             self.frame1 = CTkFrame(self, width=400) 
@@ -113,17 +147,23 @@ else:
             self.create_region()
             
             if not install_font_result:
-                self.show_notification(text.inAppText['font_error_info'], title=text.inAppText['font_error'], func=self.open_folder)
+                self.show_notification(text.inAppText['font_error_info'], title=text.inAppText['font_error'], func=self.open_folder, button=text.inAppText['fix_manually'], _type='error')
 
         def create_region(self, region=None):
             self.frame2.destroy()
             self.frame2 = CTkFrame(self, width=400)
             lbl_hi = CTkLabel(self.frame2, text=text.inAppText['region'], font=(FONT, 15))
             lbl_hi.pack(padx=10, pady=15, side=LEFT)
+
+            update_logo = CTkImage(light_image=Image.open(DIRECTORY+"data/update_icon.png"), size=(20, 20))
+            self.settings_button = CTkButton(self.frame2, text='', command=self.update_prog, width=25, height=25, image=update_logo)
+            self.settings_button.pack(side="right", padx=(0, 10), pady=1)
+
             reg = CTkOptionMenu(self.frame2, values=[text.inAppText['ru'], text.inAppText['other']],
-                                                            command=self.change_region, width=400)
-            reg.pack(padx=10, pady=15, side=RIGHT, fill="both")
+                                                            command=self.change_region, width=200)
+            reg.pack(padx=10, pady=15, side=RIGHT, fill='both')
             reg.set(region if region else self.region)
+            
 
             self.switch_var = StringVar(value="on" if self.process else "off")
 
@@ -173,23 +213,38 @@ else:
             self.frame1.pack(pady=10, padx=10, fill="both", expand=True)
 
         def update_blacklist_thread(self):
+            return_code = 0
             self.is_update = True 
             was_running = self.stop_process()
-            self.show_notification(text.inAppText['update_in_process_info'], text.inAppText['update_in_process'])
+            progress_toast = ProgressToast('GoodbyeDPI_app', text.inAppText['update_in_process'], text.inAppText['update_in_process_info'], 'russia_blacklist.txt')
             try:
-                download_blacklist("https://p.thenewone.lol/domains-export.txt")
+                download_blacklist("https://p.thenewone.lol/domains-export.txt", progress_toast)
             except Exception as ex:
-                self.show_notification(f"{ex}",title=text.inAppText['error'])
+                self.show_notification(f"{ex}",title=text.inAppText['error'], func=self.update_txt, _type='error')
+                return_code = 1
                 
             self.is_update = False 
             if was_running:
                 self.start_process()
             
-            self.show_notification(text.inAppText['update_complete'])
+            if return_code == 0:self.show_notification(text.inAppText['update_complete'])
 
         def update_txt(self):
             update_thread = threading.Thread(target=self.update_blacklist_thread)
-            update_thread.start()                
+            update_thread.start()   
+
+        def update_prog(self):
+            if not self.is_update:
+                update_version = get_latest_release()
+                if version != update_version:
+                    result = messagebox.askyesno('GoodbyeDPI UI', text.inAppText['ask_update_prog'])
+                    if result:
+                        self.stop_process()
+                        move_settings_file(SETTINGS_FILE_PATH, BACKUP_SETTINGS_FILE_PATH)
+                        subprocess.Popen('update.exe -l '+settings.settings['GLOBAL']['language']+f' -v {update_version}')
+                
+                else:
+                    messagebox.showinfo('GoodbyeDPI UI', text.inAppText['lastest_version'])
 
         def change_region(self, region):
             _region = settings.settings['REGION']['region']
@@ -235,9 +290,9 @@ else:
                     self.process = start_process(*_args)
                     self.show_notification(text.inAppText['process']+" goodbyedpi.exe " + text.inAppText['run_comlete'])
                 else:
-                    self.show_notification(f"Cannot run process goodbyedpi.exe while updating is running", title=text.inAppText['error'])
+                    self.show_notification(f"Cannot run process goodbyedpi.exe while updating is running", title=text.inAppText['error'], func=self.start_process, _type='error')
             except Exception as ex:
-                self.show_notification(f"{ex}", title=text.inAppText['error'])           
+                self.show_notification(f"{ex}", title=text.inAppText['error'], func=self.start_process, _type='error')           
         
         def stop_process(self):
             if self.process:
@@ -249,11 +304,12 @@ else:
                             self.process = None
                             return True
                         except psutil.NoSuchProcess:
-                            self.show_notification((text.inAppText['close_error'] + " goodbyedpi.exe. " + text.inAppText['close_error2']) , title=text.inAppText['error_title'] )
-                self.show_notification(text.inAppText['close_error'] +" goodbyedpi.exe. " + text.inAppText['close_error1'], title=text.inAppText['error_title'] )
+                            self.show_notification((text.inAppText['close_error'] + " goodbyedpi.exe. " + text.inAppText['close_error2']) , title=text.inAppText['error_title'], func=self.stop_process, _type='error')
+                self.show_notification(text.inAppText['close_error'] +" goodbyedpi.exe. " + text.inAppText['close_error1'], title=text.inAppText['error_title'], func=self.stop_process, _type='error')
         
         def on_closing(self):
-            if self.process: self.stop_process() 
+            if not DEBUG:
+                if self.process: self.stop_process() 
             if not self.is_update: self.destroy()
             else:self.show_notification(f"Cannot close application while updating is running", title=text.inAppText['error'])
 
@@ -275,9 +331,9 @@ else:
         def show_window(self, icon=None, item=None):
             if self.tray_icon:
                 self.tray_icon.stop()
-            self.after(0, self.deiconify)
-            self.after(0, self.lift)
-            self.after(0, self.focus_force)
+            self.after(10, self.deiconify)
+            self.after(10, self.lift)
+            self.after(10, self.focus_force)
             
 
         def exit_app(self, icon, item):
@@ -312,7 +368,7 @@ else:
                 self.show_notification(text.inAppText['autorun_complete'])
                 self.create_region()
             except Exception as ex:
-                self.show_notification(f"{ex}", title=text.inAppText['autorun_error'] )
+                self.show_notification(f"{ex}", title=text.inAppText['autorun_error'], func=self.install_service, _type='error')
 
         def remove_service(self):
             try:
@@ -331,20 +387,20 @@ else:
                 self.show_notification(text.inAppText['autorun_complete1'])
                 self.create_region()
             except Exception as ex:
-                self.show_notification(f"{ex}", title=text.inAppText['autorun_error1'])
-        
+                self.show_notification(f"{ex}", title=text.inAppText['autorun_error1'], func=self.remove_service, _type='error')
 
-        def show_notification(self, message, title="GoodbyeDPI UI", func=None):
-            print(message, title)
-            self.notifier = ToastNotifier()
-            self.notifier.show_toast(
-            title,
-            str(message),
-            icon_path=DIRECTORY+"data/icon.ico",
-            duration=10,
-            threaded=True,
-            callback_on_click=self.on_notification_click if func == None else func
-            )
+        def show_notification_tread(self, title="GoodbyeDPI UI", message='123', button=None, func=None, _type='normal'):
+            if _type=='normal':
+                result = asyncio.run(show_message("GoodbyeDPI_app", title, message))
+            else:
+                result = asyncio.run(show_error("GoodbyeDPI_app", title, message, button if button else text.inAppText['retry']))
+            if result.dismiss_reason == ToastDismissReason.NOT_DISMISSED:
+                if func:
+                    func()
+
+        def show_notification(self, message, title="GoodbyeDPI UI", func=None, button=None, _type='normal'):
+            self.notification_thread = threading.Thread(target=lambda: self.show_notification_tread(title, message, func=func, button=button, _type=_type))
+            self.notification_thread.start() 
 
         def on_notification_click(self):
             self.show_window(None, None)
