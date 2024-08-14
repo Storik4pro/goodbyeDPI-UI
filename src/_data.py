75b06b2754d1a1c622d25ad2f798cf5274f77631
Storik4 <serstor15@gmail.com>
2024-08-14 19:32:45 +0300

    adding settings.ini backup support

--- src/_data.py
+++ src/_data.py
@@ -6,9 +6,13 @@ DEBUG = False
 DIRECTORY = f'{os.path.dirname(os.path.abspath(__file__))}/' if not DEBUG else ''
 
 SETTINGS_FILE_PATH = DIRECTORY+'data/settings/settings.ini'
+BACKUP_SETTINGS_FILE_PATH = DIRECTORY+'data/settings/_settings.ini'
 LOCALE_FILE_PATH =  DIRECTORY+'data/loc/loc.ini'
 GOODBYE_DPI_PATH =  DIRECTORY+'data/goodbyeDPI/'
 FONT = 'Nunito SemiBold'
+
+REPO_OWNER = "Storik4pro"
+REPO_NAME = "goodbyeDPI-UI"
 
 class Settings:
     def __init__(self) -> None:
