282f8597b99ef2e1e605d33e9ee1915c9003e925
Storik4 <serstor15@gmail.com>
2024-08-24 09:07:29 +0300

    dropdown added

---
+++ src/Elements/__init__.py
@@ -0,0 +1,12 @@
+"""
+CustomTkinter Scrollable Dropdown Menu
+Author: Akash Bora
+License: MIT
+This is a custom dropdown menu for customtkinter.
+Homepage: https://github.com/Akascape/CTkScrollableDropdown
+"""
+
+__version__ = '1.2'
+
+from .ctk_scrollable_dropdown import CTkScrollableDropdown
+
