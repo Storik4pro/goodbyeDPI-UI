4c92df9798ef6916e145271de5a43ca4766d2fb0
Storik4 <serstor15@gmail.com>
2024-09-21 17:07:25 +0300

    added new features, fixing update critical bug

--- src/_data.py
+++ src/_data.py
@@ -1,9 +1,17 @@
 import configparser
 import os
+import sys
 
-DEBUG = False
+DEBUG = True
 
-DIRECTORY = f'{os.path.dirname(os.path.abspath(__file__))}/' if not DEBUG else ''
+if getattr(sys, 'frozen', False):
+    application_path = os.path.dirname(sys.executable)
+else:
+    application_path = os.path.dirname(os.path.abspath(__file__))
+
+DIRECTORY = f'{application_path}/' if not DEBUG else ''
+
+VERSION = "1.1.0"
 
 SETTINGS_FILE_PATH = DIRECTORY+'data/settings/settings.ini'
 BACKUP_SETTINGS_FILE_PATH = DIRECTORY+'data/settings/_settings.ini'
@@ -11,12 +19,50 @@ LOCALE_FILE_PATH =  DIRECTORY+'data/loc/loc.ini'
 GOODBYE_DPI_PATH =  DIRECTORY+'data/goodbyeDPI/'
 FONT = 'Nunito SemiBold'
 
+PARAMETER_MAPPING = {
+    'blockpassivedpi': '-p',
+    'blockquic': '-q',
+    'replacehost': '-r',
+    'removespace': '-s',
+    'mixhostcase': '-m',
+    'donotwaitack': '-n',
+    'additionalspace': '-a',
+    'processallports': '-w',
+    'allownosni': '--allow-no-sni',
+    'dnsverbose': '--dns-verb',
+    'wrongchecksum': '--wrong-chksum',
+    'wrongseq': '--wrong-seq',
+    'nativefrag': '--native-frag',
+    'reversefrag': '--reverse-frag',
+    'blacklist': '--blacklist',
+}
+VALUE_PARAMETERS = {
+    'dns': '--dns-addr',
+    'dns_port': '--dns-port',
+    'dnsv6': '--dnsv6-addr',
+    'dnsv6_port': '--dnsv6-port',
+    'httpfragmentation': '-f',
+    'httpkeepalive': '-k',
+    'httpsfragmentation': '-e',
+    'maxpayload': '--max-payload',
+    'additionalport': '--port',
+    'ipid': '--ip-id',
+    'fakefromhex': '--fake-from-hex',
+    'fakegen': '--fake-gen',
+    'fakeresend': '--fake-resend',
+    'autottl': '--auto-ttl',
+    'minttl': '--min-ttl',
+    'setttl': '--set-ttl',
+        
+}
+
+
 REPO_OWNER = "Storik4pro"
 REPO_NAME = "goodbyeDPI-UI"
 
 class Settings:
     def __init__(self) -> None:
-        self.reload_settings()
+        self.settings = self.reload_settings()
     
     def reload_settings(self):
         config = configparser.ConfigParser()
