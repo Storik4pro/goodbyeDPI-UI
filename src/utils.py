07fcbbcd2a6733f8d5bb1350ed3774b7b05f340d
Storik4 <serstor15@gmail.com>
2024-10-05 17:50:06 +0300

    fixed bug

--- src/utils.py
+++ src/utils.py
@@ -502,7 +502,7 @@ def sni_support():
         return False 
     
 def check_urls():
-    with open(f"{'E:/ByeDPI' if not DEBUG else '' + GOODBYE_DPI_PATH}/custom_blacklist.txt", 'r') as file:
+    with open(f"{'E:/ByeDPI' if DEBUG else '' + GOODBYE_DPI_PATH}/custom_blacklist.txt", 'r') as file:
         urls = file.read().splitlines()
 
     sites = []
