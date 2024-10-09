2021bc27a9feb83fbbdc07ed40b9648f677e8420
Storik4 <serstor15@gmail.com>
2024-10-09 12:59:27 +0300

    ✨VERSION updated to 1.1.4

--- src/_data.py
+++ src/_data.py
@@ -11,7 +11,7 @@ else:
 
 DIRECTORY = f'{application_path}/_internal/' if not DEBUG else ''
 
-VERSION = "1.1.3"
+VERSION = "1.1.4"
 
 SETTINGS_FILE_PATH = DIRECTORY+'data/settings/settings.ini'
 BACKUP_SETTINGS_FILE_PATH = DIRECTORY+'data/settings/_settings.ini'
