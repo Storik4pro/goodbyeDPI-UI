46f73f3ad72a9adea22becf13eed605ca792e928
Storik4 <serstor15@gmail.com>
2024-10-24 10:15:22 +0300

    🚀Refactoring code for better performance

--- src/main.py
+++ src/main.py
@@ -17,9 +17,10 @@ from tkinter.font import Font
 import ctypes.wintypes
 import darkdetect
 from customtkinter import *
-from _data import LOG_LEVEL, VERSION, settings, SETTINGS_FILE_PATH, GOODBYE_DPI_PATH, FONT, DEBUG, DIRECTORY, REPO_NAME, REPO_OWNER, BACKUP_SETTINGS_FILE_PATH, text
-from utils import check_mica, install_font, register_app, is_process_running, change_setting
-from quick_start import kill_update, merge_settings, merge_blacklist, rename_update_exe
+from _data import BYEDPI_EXECUTABLE, LOG_LEVEL, SPOOFDPI_EXECUTABLE, VERSION, ZAPRET_EXECUTABLE, settings,\
+      SETTINGS_FILE_PATH, GOODBYE_DPI_PATH, FONT, DEBUG, DIRECTORY, REPO_NAME, REPO_OWNER, BACKUP_SETTINGS_FILE_PATH, text
+from utils import check_mica, install_font, is_weak_pc, register_app, is_process_running, change_setting
+from quick_start import kill_update, merge_settings, merge_blacklist, rename_update_exe, merge_settings_to_json
 from logger import AppLogger
 import pywintypes
 import configparser
@@ -38,31 +39,48 @@ def is_admin():
         return ctypes.windll.shell32.IsUserAnAdmin()
     except:
         return False
-
-def check_first_run():
-    first_run = settings.settings['GLOBAL']['is_first_run']
-    install_font_result = True  
+    
+def check_app_is_runned():
     if not DEBUG:
         existing_app = is_process_running('goodbyeDPI.exe')
         if existing_app:
-            result = messagebox.askyesno(text.inAppText['app_is_running'], text.inAppText['process']+" 'goodbyeDPI.exe' "+text.inAppText['app_is_running_info'])
+            result = messagebox.askyesno(text.inAppText['app_is_running'], 
+                                         text.inAppText['process']+" 'goodbyeDPI.exe' "+text.inAppText['app_is_running_info'])
             if result:
                 try:
                     existing_app.terminate()
                     existing_app.wait()
+                except psutil.NoSuchProcess:
+                    logger.create_debug_log(traceback.format_exc())
                 except:
                     logger.create_error_log(traceback.format_exc())
             else:
                 sys.exit(0)
-        if first_run == 'True':
-            register_app()
-            install_font_result = install_font('data/font/Nunito-SemiBold.ttf')
-            settings.reload_settings()
-    return install_font_result
+
+def first_run_actions():
+    first_run = settings.settings.getboolean('GLOBAL', 'is_first_run')
+    if first_run:
+        register_app()
+        install_font('data/font/Nunito-SemiBold.ttf')
+
+        settings.change_setting('GLOBAL', 'is_first_run', 'False')
+
+        if is_weak_pc():
+            settings.change_setting('APPEARANCE_MODE', 'animations', 'False')
+
+def after_update_actions():
+    merge_settings_to_json()
+    try:
+        kill_update()
+        update_result = rename_update_exe()
+        settings.change_setting('GLOBAL', 'update_complete', "True")
+    except:
+        logger.create_error_log(traceback.format_exc())
+        settings.change_setting('GLOBAL', 'update_complete', "False")
 
 def chk_directory():
     if settings.settings['GLOBAL']["work_directory"] != DIRECTORY and not "System32" in DIRECTORY:
-        change_setting('GLOBAL', 'work_directory', DIRECTORY)
+        settings.change_setting('GLOBAL', 'work_directory', DIRECTORY)
     
 if __name__ == "__main__":
     if not DEBUG: multiprocessing.freeze_support()
@@ -73,15 +91,16 @@ if __name__ == "__main__":
         pass
 
     autorun = 'False'
-    after_update = 'False'
+    after_update = False
     first_run = settings.settings['GLOBAL']['is_first_run'] if not DEBUG else 'False'
     pompt = ' '
+
     for name, value in options:
         if name == '--autorun':
             autorun = 'True'
         if name == '--after-update':
-            after_update = 'True'
-            change_setting('GLOBAL', 'update_complete', "False")
+            after_update = True
+            settings.change_setting('GLOBAL', 'update_complete', "False")
         pompt+=name+value
     
     logger.create_debug_log("Getting ready for start application.")
@@ -92,48 +111,68 @@ if __name__ == "__main__":
         logger.cleanup_logs()
     else:
         logger.create_debug_log("Started as administrator")
-        if after_update == 'True':
-            merge_settings(BACKUP_SETTINGS_FILE_PATH, SETTINGS_FILE_PATH)
-            merge_blacklist(GOODBYE_DPI_PATH)
-            settings.reload_settings()
-            change_setting('GLOBAL', 'after_update', 'False')
 
-        if (after_update == 'True' or first_run == 'True' or settings.settings['GLOBAL']['update_complete'] == 'False') and not DEBUG:
-            try:
-                kill_update()
-                update_result = rename_update_exe()
-                change_setting('GLOBAL', 'update_complete', "True")
-            except:
-                logger.create_error_log(traceback.format_exc())
-                change_setting('GLOBAL', 'update_complete', "False")
-        if first_run == 'True':
-            change_setting('GLOBAL', 'work_directory', DIRECTORY)
-        
-        set_default_color_theme("blue")
-
-        install_font_result = check_first_run()
+        check_app_is_runned()
 
         if settings.settings['GLOBAL']['hide_to_tray'] == "True":
             autorun = 'True'
+        try:
+            if not DEBUG:
+                # first run actions
+                first_run_actions()
 
-        window = MainWindow(install_font_result, autorun, after_update if after_update == 'True' else first_run)
+                # check work directory 
+                chk_directory()
 
-        if not DEBUG:
-            config = configparser.ConfigParser()
-            config.read(SETTINGS_FILE_PATH)
-            config['GLOBAL']['is_first_run'] = 'False'
-            with open(SETTINGS_FILE_PATH, 'w') as configfile:
-                config.write(configfile)
+                # check components installed
+                config = settings.settings
+                config.set('GLOBAL', 'is_first_run', 'False')
 
+                components_to_check = {
+                    'spoofdpi': SPOOFDPI_EXECUTABLE,
+                    'byedpi': BYEDPI_EXECUTABLE,
+                    'zapret':ZAPRET_EXECUTABLE,
+                }
+
+                for component, executable in components_to_check.items():
+                    component_path = os.path.join(DIRECTORY, "data", component, executable)
+                    if config.getboolean('COMPONENTS', component) and not os.path.exists(component_path):
+                        settings.change_setting('COMPONENTS', component, 'False')
+                        logger.create_info_log(f'Component {component} will be unregistered, because {executable} not exist')
+
+                # check after update actions
+                if not settings.settings.getboolean('GLOBAL', 'update_complete'):
+                    after_update_actions()
+
+                # merge settings  
+                if after_update == 'True':
+                    merge_settings(BACKUP_SETTINGS_FILE_PATH, SETTINGS_FILE_PATH)
+                    merge_blacklist(GOODBYE_DPI_PATH)
+
+                    settings.change_setting('GLOBAL', 'after_update', 'False')
+            
+            settings.save_settings()
+        except:
+            logger.raise_critical(traceback.format_exc())
+        
+        # getting ready window
         mode = settings.settings['APPEARANCE_MODE']['mode']
         mica = settings.settings['APPEARANCE_MODE']['use_mica'] if check_mica() else "False"
-        
-        set_appearance_mode(mode if mica != "True" else "dark")
+
+        set_default_color_theme("blue")
         set_widget_scaling(1)
+        set_appearance_mode(mode if mica != "True" else "dark")
+
+        window = MainWindow(autorun, 'True' if after_update else first_run)
+
         try:
             window.iconbitmap(DIRECTORY+'data/icon.ico')
-            if mica == "True": ApplyMica(windll.user32.FindWindowW(c_char_p(None), window.title()), True, False)
+
+            if mica == "True": 
+                ApplyMica(windll.user32.FindWindowW(c_char_p(None), window.title()), True, False)
+
             window.mainloop()
         except: pass
+
         logger.cleanup_logs()
         
