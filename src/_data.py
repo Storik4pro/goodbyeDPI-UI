1f9742c280d3209bfbdd93476079bd1c6fd455bc
Storik4 <serstor15@gmail.com>
2024-10-24 10:09:56 +0300

    ✨added new DEBUG features, font check util, config support

--- src/_data.py
+++ src/_data.py
@@ -1,29 +1,65 @@
 import configparser
+import json
 import logging
 import os
 import sys
 import traceback
+import winreg
 from logger import AppLogger
 
 DEBUG = False
+DEBUG_PATH = os.path.dirname(os.path.abspath(__file__)).replace("\src", "/")
 
 if getattr(sys, 'frozen', False):
     application_path = os.path.dirname(sys.executable)
 else:
-    application_path = os.path.dirname(os.path.abspath(__file__))
+    application_path = DEBUG_PATH
+
+def is_font_installed(font_name):
+    try:
+        registry_key = winreg.OpenKey(winreg.HKEY_LOCAL_MACHINE, r"SOFTWARE\Microsoft\Windows NT\CurrentVersion\Fonts")
+        i = 0
+        while True:
+            try:
+                value = winreg.EnumValue(registry_key, i)
+                if font_name.lower() in value[0].lower():
+                    return True
+                i += 1
+            except OSError:
+                break
+        return False
+    except Exception:
+        return False
 
 DIRECTORY = f'{application_path}/_internal/' if not DEBUG else ''
 
-VERSION = "1.1.6"
+VERSION = "1.2.0"
 
 SETTINGS_FILE_PATH = DIRECTORY+'data/settings/settings.ini'
 BACKUP_SETTINGS_FILE_PATH = DIRECTORY+'data/settings/_settings.ini'
+CONFIG_PATH = DIRECTORY+'data/settings/presets'
 LOCALE_FILE_PATH =  DIRECTORY+'data/loc/loc.ini'
 GOODBYE_DPI_PATH =  DIRECTORY+'data/goodbyeDPI/'
 GOODBYE_DPI_EXECUTABLE = "goodbyedpi.exe" 
 ZAPRET_PATH = DIRECTORY+'data/zapret/'
 ZAPRET_EXECUTABLE = "winws.exe" 
-FONT = 'Nunito SemiBold'
+SPOOFDPI_EXECUTABLE = "spoofdpi-windows-amd64.exe" 
+BYEDPI_EXECUTABLE = "ciadpi.exe" 
+EXECUTABLES = {
+    'goodbyeDPI':GOODBYE_DPI_EXECUTABLE,
+    'zapret':ZAPRET_EXECUTABLE,
+    'spoofdpi':SPOOFDPI_EXECUTABLE,
+    'byedpi': BYEDPI_EXECUTABLE
+}
+COMPONENTS_URLS = {
+    'goodbyeDPI':'ValdikSS/GoodbyeDPI',
+    'zapret':'bol-van/zapret',
+    'spoofdpi':'xvzc/SpoofDPI',
+    'byedpi':'hufrea/byedpi'
+}
+
+FONT = 'Nunito SemiBold' if is_font_installed('Nunito SemiBold') else 'Segoe UI'
+MONO_FONT = 'Cascadia Mono' if is_font_installed('Cascadia Mono') else 'Consolas'
 
 PARAMETER_MAPPING = {
     'blockpassivedpi': '-p',
@@ -64,23 +100,42 @@ VALUE_PARAMETERS = {
 
 S_PARAMETER_MAPPING = {
     'enabledoh': '-enable-doh',
+    'ipv4only': '-dns-ipv4-only',
+    'silent': '-silent',
+    'systemproxy': '-system-proxy',
+    'enabledebug': '-debug',
 }
 
 S_VALUE_PARAMETERS = {
-    'sdns_port': '-dns-port',
-    "sdns": "-dns-addr",
-    "windowsize": '-window-size',
+    'addr': '-addr',
+    'dns': '-dns-addr',
+    'dnsport': '-dns-port',
+    'port': '-port',
+    'pattern': '-pattern',
+    'timeout': '-timeout',
+    'windowsize': '-window-size',
 }
+
 
 
 REPO_OWNER = "Storik4pro"
 REPO_NAME = "goodbyeDPI-UI"
+CONFIGS_REPO_NAME = "goodbyeDPI-UI-configs"
 
 logger = AppLogger(VERSION, "settings_import")
 
 class Settings:
     def __init__(self) -> None:
         self.settings = self.reload_settings()
+
+    def change_setting(self, group, key, value):
+        self.settings[group][key] = value
+
+    def save_settings(self):
+        with open(SETTINGS_FILE_PATH, 'w', encoding='utf-8') as configfile:
+            self.settings.write(configfile)
+
+        self.reload_settings()
     
     def reload_settings(self):
         config = configparser.ConfigParser()
@@ -110,6 +165,57 @@ try:
     logger.create_logs(f"Importing application localize from \"{LOCALE_FILE_PATH}\"")
     text = Text(settings.settings['GLOBAL']['language'])
 except Exception as ex: 
+    error_message = traceback.format_exc()
+    logger.raise_critical(error_message)
+
+class UserConfig:
+    def __init__(self, configfile) -> None:
+        self.configfile = configfile
+        self.data = self.reload_config()
+
+    def reload_config(self):
+        with open(self.configfile, 'r', encoding='utf-8') as file:
+            self.data = json.load(file)
+        return self.data
+
+    def write_config(self):
+        with open(self.configfile, 'w', encoding='utf-8') as file:
+            json.dump(self.data, file, ensure_ascii=False, indent=4)
+        self.data = self.reload_config()
+
+    def copy_to(self, path):
+        with open(path, 'w', encoding='utf-8') as file:
+            json.dump(self.data, file, ensure_ascii=False, indent=4)
+
+    def get_value(self, key) -> str|bool|int:
+        return self.data.get(key)
+    
+    def set_value(self, key, value):
+        print(value, key)
+        self.data[key] = value
+        self.write_config()
+
+try:
+    goodbyedpi_config_path = os.path.join(settings.settings['CONFIG']['goodbyedpi_config_path'])
+    zapret_config_path = os.path.join(settings.settings['CONFIG']['zapret_config_path'])
+    byedpi_config_path = os.path.join(settings.settings['CONFIG']['byedpi_config_path'])
+    spoofdpi_config_path = os.path.join(settings.settings['CONFIG']['spoofdpi_config_path'])
+    
+    configs = {
+        'goodbyedpi':UserConfig(CONFIG_PATH+"/goodbyedpi/user.json" if\
+                                goodbyedpi_config_path == "" or not os.path.exists(goodbyedpi_config_path) else\
+                                goodbyedpi_config_path),
+        'zapret':UserConfig(CONFIG_PATH+"/zapret/user.json" if\
+                            zapret_config_path == "" or not os.path.exists(zapret_config_path) else\
+                            zapret_config_path),
+        'byedpi':UserConfig(CONFIG_PATH+"/byedpi/user.json" if\
+                            byedpi_config_path == "" or not os.path.exists(byedpi_config_path) else\
+                            byedpi_config_path),
+        'spoofdpi':UserConfig(CONFIG_PATH+"/spoofdpi/user.json" if\
+                            spoofdpi_config_path == "" or not os.path.exists(spoofdpi_config_path) else\
+                            spoofdpi_config_path)
+    }
+except:
     error_message = traceback.format_exc()
     logger.raise_critical(error_message)
 
