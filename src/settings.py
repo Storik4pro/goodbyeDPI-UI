c77806b4b3dd2f8fd7a5f16a4eb7052b621d1730
Storik4 <serstor15@gmail.com>
2024-11-01 17:33:56 +0300

    🗑 removed tkinter windows

--- src/settings.py
+++
@@ -1,9 +0,0 @@
-from _settings.Gallery.main import run_qt_app
-from multiprocessing import Pipe, Process
-
-
-def start_qt_settings():
-    parent_conn, child_conn = Pipe()
-    p = Process(target=run_qt_app, args=(child_conn,))
-    p.start()
-    return parent_conn, p
