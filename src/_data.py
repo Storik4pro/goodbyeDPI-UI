ba0a6b1c53057e20cda1464e7089ac9141515be1
Storik4pro <serstor15@gmail.com>
2025-06-05 14:45:18 +0300

    ⬆️ VERSION updated to 1.2.11

--- src/_data.py
+++ src/_data.py
@@ -43,7 +43,7 @@ def is_font_installed(font_name):
 
 DIRECTORY = f'{application_path}/' if not DEBUG else ''
 
-VERSION = "1.2.10"
+VERSION = "1.2.11"
 
 load_dotenv()
 DEV_API = os.getenv('DEV_API')
